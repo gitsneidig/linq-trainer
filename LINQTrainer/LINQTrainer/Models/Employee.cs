146198c3e72cd23bc3ba6a0ea72117bb0c3bcd50
gitsneidig <scott@scottneidig.com>
2021-03-05 14:31:54 -0600

    Added Employees class

---
+++ LINQTrainer/LINQTrainer/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQTrainer
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
