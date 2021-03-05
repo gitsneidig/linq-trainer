5b76188fe0fbd183faafc17e7db1f0d0e43c075c
gitsneidig <scott@scottneidig.com>
2021-03-05 15:44:59 -0600

    Extension method example

---
+++ LINQTrainer/LINQTrainer/MyLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQTrainer
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
