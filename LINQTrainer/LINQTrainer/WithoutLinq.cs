8f83d130642aa9dbf2ddd6ce76c31aada3ec2824
gitsneidig <scott@scottneidig.com>
2021-03-05 14:32:16 -0600

    Added basic IEnumerator example

--- LINQTrainer/LINQTrainer/WithoutLinq.cs
+++ LINQTrainer/LINQTrainer/WithoutLinq.cs
@@ -11,6 +11,8 @@ namespace LINQTrainer
     public class WithoutLinq
     {
 
+        public List<Employee> employees = new List<Employee>();
+
         public static void ShowLargeFilesWithoutLinq(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
@@ -31,6 +33,47 @@ namespace LINQTrainer
                 return y.Length.CompareTo(x.Length);
             }
         }
+
+
+        public void ShowScottListLegacy()
+        {
+
+            IEnumerable<Employee> employees = new List<Employee>()
+            {
+                new Employee { firstName = "Scott", lastName = "Alen" },
+                new Employee { firstName = "Bob", lastName = "Martin" }
+            };
+
+            /*/
+            // EnumerableExtensions is in namespace Microsoft.Practices.ObjectBuildedr2
+            // and no longer support/retired
+            // The closes thing to LINQ that existed in C# 2.0
+            IEnumerable<Employee> scottList =
+                EnumerableExtensions.Where(employees,
+                    delegate (Employee scott)
+                    {
+                        Console.WriteLine($"{scott.firstName} {scott.lastName} \n");
+                    });
+            /*/
+        }
+
+        public void IEnumeratorExample()
+        {
+
+            employees.Add(new Employee { id = 1, firstName = "Scott", lastName = "Alen" });
+            employees.Add(new Employee { id = 2, firstName = "Bob", lastName = "Martin" });
+
+            IEnumerator<Employee> enumerator = employees.GetEnumerator();
+
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine($"{enumerator.Current.firstName} {enumerator.Current.lastName}");
+            }
+
+
+
+        }
+
 
     }
 }
