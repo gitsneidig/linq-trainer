0af1ed36f4d60c8252c3c4409d1215573f6eab29
gitsneidig <scott@scottneidig.com>
2021-03-04 21:17:50 -0600

    Sorting a list of files in a directory WITHOUT using LINQ

---
+++ LINQTrainer/LINQTrainer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace LINQTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for(int i = 0; i < 5; i++) 
+            //foreach(FileInfo file in files)
+            {
+                Console.WriteLine($"{files[i].Name, -20}: {files[i].Length, 10:N0}");
+            }
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+    }
+}
