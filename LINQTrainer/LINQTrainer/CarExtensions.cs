8885bc5284c44e0ee7a93bcd7309bf7cd8c85de9
gitsneidig <scott@scottneidig.com>
2021-03-08 13:35:25 -0600

    Added code for to project CSV line items to objects

---
+++ LINQTrainer/LINQTrainer/CarExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LINQTrainer.Models;
+
+namespace LINQTrainer
+{
+    public static  class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+
+
+    }
+}
