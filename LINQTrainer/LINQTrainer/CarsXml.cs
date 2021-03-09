b347fdac2c6afcdf74f7fa6698cf3ffacded93a9
gitsneidig <scott@scottneidig.com>
2021-03-08 18:11:30 -0600

    Generate XML, with LINQ select operator for projection

--- LINQTrainer/LINQTrainer/CarsXml.cs
+++ LINQTrainer/LINQTrainer/CarsXml.cs
@@ -16,6 +16,16 @@ namespace LINQTrainer
             var document = new XDocument();
             var carsElem = new XElement("Cars");
 
+            // Syntax 3
+            var elements = 
+                from item in queryResult
+                select new XElement("Car",
+                          new XElement("Name", item.Name),
+                          new XElement("Combined", item.Combined));
+
+
+            // Syntax 2
+            /*/
             foreach (var item in queryResult.Take(10))
             {
 
@@ -24,21 +34,25 @@ namespace LINQTrainer
                           new XElement("Combined", item.Combined));
 
                 carsElem.Add(car);
+            /*/
 
-                /*/
-                var car = new XElement("Car");
-                var name = new XElement("Name", item.Name);
-                var combined = new XElement("Combined", item.Combined);
+            // Syntax 1               
+            /*/
+            var car = new XElement("Car");
+            var name = new XElement("Name", item.Name);
+            var combined = new XElement("Combined", item.Combined);
 
-                carsElem.Add(name);
-                carsElem.Add(combined);
-                carsElem.Add(car);
-                /*/
+            carsElem.Add(name);
+            carsElem.Add(combined);
+            carsElem.Add(car);
 
-            }
 
-            document.Add(carsElem);
-            document.Save("fuel.xml");
+        }
+
+        document.Add(carsElem);
+        document.Save("fuel.xml");
+
+        /*/
 
         }
 
